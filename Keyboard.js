b648a996ba6729f43f0dd9dcbc5de3d700521a39
Brandon Benvie <brandon@bbenvie.com>
2012-08-04 20:07:38 -0400

    super accurify the keys

--- Keyboard.js
+++ Keyboard.js
@@ -1,29 +1,210 @@
 void function(){
 
-function whatKey(evt){
-  if (keymap[evt.charCode]) {
-    return keymap[evt.charCode];
-  } else {
-    return String.fromCharCode(evt.charCode);
-  }
-}
-
-var keymap = {
-  8: "backspace", 9: "tab", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
-  20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
-  37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del",
-  96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
-  104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/",
-  112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8",
-  120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 191: "/", 224: "meta"
+var keys = {
+    0: ['Unknown'],
+    1: ['Mouse 1'],
+    2: ['Mouse 2'],
+    3: ['Break'],
+    4: ['Mouse 3'],
+    5: ['Mouse 4'],
+    6: ['Mouse 5'],
+    8: ['Backspace'],
+    9: ['Tab'],
+   12: ['Clear'],
+   13: ['Return'],
+   16: ['Shift'],
+   17: ['Control'],
+   18: ['Alt'],
+   19: ['Pause'],
+   20: ['CapsLock'],
+   21: ['IME Hangul'],
+   23: ['IME Junja'],
+   24: ['IME Final'],
+   25: ['IME Kanji'],
+   27: ['Escape'],
+   28: ['IME Convert'],
+   29: ['IME Nonconvert'],
+   30: ['IME Accept'],
+   31: ['IME Modechange'],
+   32: ['Space'],
+   33: ['PageUp'],
+   34: ['PageDown'],
+   35: ['End'],
+   36: ['Home'],
+   37: ['Left'],
+   38: ['Up'],
+   39: ['Right'],
+   40: ['Down'],
+   41: ['Select'],
+   42: ['Print'],
+   43: ['Execute'],
+   44: ['Snapshot'],
+   45: ['Insert'],
+   46: ['Delete'],
+   47: ['Help'],
+   48: '0)',
+   49: '1!',
+   50: '2@',
+   51: '3#',
+   52: '4$',
+   53: '5%',
+   54: '6^',
+   55: '7&',
+   56: '8*',
+   57: '9(',
+   65: 'aA',
+   66: 'bB',
+   67: 'cC',
+   68: 'dD',
+   69: 'eE',
+   70: 'fF',
+   71: 'gG',
+   72: 'hH',
+   73: 'iI',
+   74: 'jJ',
+   75: 'kK',
+   76: 'lL',
+   77: 'mM',
+   78: 'nN',
+   79: 'oO',
+   80: 'pP',
+   81: 'qQ',
+   82: 'rR',
+   83: 'sS',
+   84: 'tT',
+   85: 'uU',
+   86: 'vV',
+   87: 'wW',
+   88: 'xX',
+   89: 'yY',
+   90: 'zZ',
+   91: ['Left Win'],
+   92: ['Right Win'],
+   93: ['Menu'],
+   95: ['Sleep'],
+   96: ['Numpad 0', 'Numpad Insert'],
+   97: ['Numpad 1', 'Numpad End'],
+   98: ['Numpad 2', 'Numpad Down'],
+   99: ['Numpad 3', 'Numpad PageDown'],
+  100: ['Numpad 4', 'Numpad Left'],
+  101: ['Numpad 5', 'Numpad Clear'],
+  102: ['Numpad 6', 'Numpad Right'],
+  103: ['Numpad 7', 'Numpad Home'],
+  104: ['Numpad 8', 'Numpad Up'],
+  105: ['Numpad 9', 'Numpad PageUp'],
+  106: ['Numpad *', 'Shift Numpad *'],
+  107: ['Numpad +', 'Shift Numpad +'],
+  108: ['Numpad Separator', 'Numpad '],
+  109: ['Numpad -', 'Shift Numpad -'],
+  110: ['Numpad .', 'Numpad Delete'],
+  111: ['Numpad /', 'Shift Numpad /'],
+  112: ['F1'],
+  113: ['F2'],
+  114: ['F3'],
+  115: ['F4'],
+  116: ['F5'],
+  117: ['F6'],
+  118: ['F7'],
+  119: ['F8'],
+  120: ['F9'],
+  121: ['F10'],
+  122: ['F11'],
+  123: ['F12'],
+  124: ['F13'],
+  125: ['F14'],
+  126: ['F15'],
+  127: ['F16'],
+  128: ['F17'],
+  129: ['F18'],
+  130: ['F19'],
+  131: ['F20'],
+  132: ['F21'],
+  133: ['F22'],
+  134: ['F23'],
+  135: ['F24'],
+  144: ['NumLock'],
+  145: ['ScrollLock'],
+  160: ['Left Shift'],
+  161: ['Right Shift'],
+  162: ['Left Control'],
+  163: ['Right Control'],
+  164: ['Left Alt'],
+  165: ['Right Alt'],
+  166: ['Browser Back'],
+  167: ['Browser Forward'],
+  168: ['Browser Refresh'],
+  169: ['Browser Stop'],
+  170: ['Browser Search'],
+  171: ['Browser Favorites'],
+  172: ['Browser Home'],
+  173: ['Volume Mute'],
+  174: ['Volume Down'],
+  175: ['Volume Up'],
+  176: ['Next Track'],
+  177: ['Prev Track'],
+  178: ['Stop'],
+  179: ['Play/Pause'],
+  180: ['Launch Mail'],
+  181: ['Launch Media Select'],
+  182: ['Launch App1'],
+  183: ['Launch App2'],
+  186: ';:',
+  187: '=+',
+  188: ',<',
+  189: '-_',
+  190: '.>',
+  191: '/?',
+  192: '`~',
+  219: '[{',
+  220: '\\|',
+  221: ']}',
+  222: '\'"',
+  223: ['Meta'],
+  226: ['AltGr'],
+  229: ['IME Process'],
+  231: ['0x00'],
+  246: ['Attention'],
+  247: ['Crsel'],
+  248: ['Exsel'],
+  249: ['Erase EOF'],
+  250: ['Play'],
+  251: ['Zoom'],
+  252: ['NoName'],
+  254: ['Clear'],
 };
 
-// var shiftKeymap = {
-//   "`": "~", "1": "!", "2": "@", "3": "#", "4": "$", "5": "%", "6": "^", "7": "&",
-//   "8": "*", "9": "(", "0": ")", "-": "_", "=": "+", ";": ": ", "'": "\"", ",": "<",
-//   ".": ">",  "/": "?",  "\\": "|"
-// };
+var shiftNumpad = {
+ 33: 105,
+ 34: 99,
+ 35: 97,
+ 36: 103,
+ 37: 100,
+ 38: 104,
+ 39: 102,
+ 40: 98,
+ 45: 96,
+}
 
+function whatKey(evt){
+  var key = keys[evt.keyCode];
+  if (key) {
+    if (evt.shiftKey) {
+      return key[+evt.shiftKey] || key[0];
+    } else {
+      if (evt.keyLocation === 3) {
+        var mapped = shiftNumpad[evt.keyCode];
+        if (mapped) {
+          return keys[mapped][1];
+        }
+      }
+      return key[0];
+    }
+  } else if (evt.keyIdentifier) {
+    return evt.keyIdentifier;
+  } else {
+    return String.fromCharCode(evt.keyCode);
+  }
+}
 
 function Keyboard(view){
   var self = this;
@@ -34,30 +215,35 @@ function Keyboard(view){
   this.alt = false;
   this.meta = false;
 
-  var events = {
-    keydown: function(e){
-      self.update(e);
-      self.emit('down', e);
-    },
-    keyup: function(e){
-      self.update(e);
-      self.emit('up', e);
-    },
-    keypress: function(e){
-      self.update(e);
-      self.emit('press', e);
-    }
-  };
-
-  for (var k in events) {
-    view.addEventListener(k, events[k], true);
-  }
+  view.addEventListener('keydown', function(e){
+    self.update(e);
+    this.lastKey = e.name = whatKey(e);
+    self.emit('down', e);
+  }, true);
+  view.addEventListener('keyup', function(e){
+    self.update(e);
+    this.lastKey = e.name = whatKey(e);
+    self.emit('up', e);
+  }, true);
+  view.addEventListener('keypress', function(e){
+    self.update(e);
+    this.lastKey = e.name = String.fromCharCode(e.keyCode);
+    self.emit('press', e);
+  }, true);
 }
+
+Keyboard.LOCATION = {
+  STANDARD : 0,
+  LEFT     : 1,
+  RIGHT    : 2,
+  NUMPAD   : 3,
+  MOBILE   : 4,
+  JOYSTICK : 5
+};
 
 Keyboard.prototype = {
   constructor: Keyboard,
   update: function update(evt){
-    this.lastKey = whatKey(evt);
     this.lastEvent = evt;
     this.ctrl = evt.ctrlKey;
     this.shift = evt.shiftKey;
@@ -89,7 +275,7 @@ Keyboard.prototype = {
 
       var listeners = this.keys['*'] || (this.keys['*'] = []);
       listeners.push(function(type, evt){
-        listeners.call(self, evt);
+        listener.call(self, evt);
       });
 
     } else if (keys.length > 1) {
@@ -138,18 +324,5 @@ Keyboard.prototype = {
 };
 
 window.Keyboard = Keyboard;
-Object.defineProprerty(Window.prototype, 'keyboard', {
-  enumerate: true,
-  configurable: true,
-  get: function(){
-    var kb = new Keyboard(this);
-    Object.defineProperty(this, 'keyboard', {
-      enumerable: true,
-      configurable: true,
-      writable: true,
-      value: kb
-    });
-    return kb;
-  }
-});
+window.keyboard = new Keyboard(window);
 }();
